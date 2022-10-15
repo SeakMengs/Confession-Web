d7394ab95d9ff9aa4dd63df5957b58915a50331a
LifeGoal <54373229+SeakMengs@users.noreply.github.com>
2022-10-16 06:22:34 +0700

    finish api

--- API/api.js
+++ API/api.js
@@ -1,29 +1,45 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const port = 5500
 const router = express.Router()
+const bodyParser = require('body-parser')
 
+// ? connect db server
+mongoose.connect(
+    process.env.databaseUrl,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, res) {
+        if (err) {
+            console.warn(err);
+        }
+        try {
+            console.log('Connected to Database');
+        } catch (err) {
+            throw err
+        }
+    } 
+)
+    
 //* import from other folders------------------------------------------------------------------------------------
 const routerModule = require('./routers/router')
 app.use(routerModule)
 
-// const postModel = require('./models/models')
 //*------------------------------------------------------------------------------------
 
-// ? connect db server
-mongoose.connect(process.env.databaseUrl, {useNewUrlParser: true}, () => {
-    console.log("Connected to mongodb database server")
-})
-
-app.get('/', (res) => {
-    res.setEncoding("We are on home");
-})
+    
 // ? middleware to tell express that the server accept json
 app.use(express.json())
+app.use(bodyParser.json())
 app.use(router)
-
-// ? run api on port
+    
+    // app.get('/', (req, res) => {
+        //     res.send("Hello")
+        // })
+        
+const postModel = require('./models/models')
+        // ? run api on port
 app.listen(port, () => {
     console.log(`Listening to port ${port} `)
 })
--- API/routers/router.js
+++ API/routers/router.js
@@ -4,19 +4,21 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 // ? request models
 const postModel = require('../models/models')
+// const url = "http://localhost:5500"
 
 //Give all post info
-router.get('/', async(req, res) => {
+router.get(`/`, async(req, res) => {
     try {
-        const allPost = await  postModel.find()
+        const allPost = await postModel.find()
         res.json(allPost);
     } catch (err) {
         res.json({ message: err.message})
     }
+    // res.send("Hello hey")
 })
 
 //Give post by id
-router.get('/post/byID/:impostorId', (req, res) => {
+router.get(`/post/byID/:impostorId`, (req, res) => {
     // ? find by id
     //*example http://www.yato.com/253
     //* 253 is params
@@ -30,14 +32,16 @@ router.get('/post/byID/:impostorId', (req, res) => {
 })
 
 //Post content to api
-router.post('/', bodyParser.json(), async (req, res) => {
-    const newPost = new postModel({
-        text: req.body.text,
-        image: req.body.image,
-        comment: req.body.comment,
-        pin: req.body.pin,
-        impostorId: req.body.impostorId
-    })
+router.post(`/hey`, bodyParser.json(), async (req, res) => {
+    const newPost = new postModel(
+        {
+            text: req.body.text,
+            image: req.body.image,
+            comment: req.body.comment,
+            pin: req.body.pin,
+            impostorId: req.body.impostorId
+        }
+    )
 
     // ? save post
     try {
@@ -50,7 +54,7 @@ router.post('/', bodyParser.json(), async (req, res) => {
 })
 
 //Update data
-router.patch('/', bodyParser.json(), async(req, res) => {
+router.patch("/", bodyParser.json(), async(req, res) => {
     try {
         const updatePost = await postModel.updateOne(
             { _id: req.params._id },
@@ -71,7 +75,7 @@ router.patch('/', bodyParser.json(), async(req, res) => {
     }
 })
 
-router.delete('/post/byID/:impostorId', async(req, res) => {
+router.delete(`/post/byID/:impostorId`, async(req, res) => {
     try {
         const removePost = await postModel.deleteOne({impostorId: req.params.impostorId})
         res.json({ message: 'Remove successfully' })
