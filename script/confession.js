c68ae781dd38de506552545493cbf17bc68231f9
LifeGoal <54373229+SeakMengs@users.noreply.github.com>
2022-10-19 02:57:04 +0700

    update cmt

--- script/confession.js
+++ script/confession.js
@@ -1,6 +1,22 @@
-
 //*Credit https://stackoverflow.com/questions/17772260/textarea-auto-height
 function auto_grow(element) {
     element.style.height = "5px";
-    element.style.height = (element.scrollHeight)+"px";
+    element.style.height = (element.scrollHeight) + "px";
+}
+
+function sendCmt(event) {
+    var impId = event.replace('send#', '');
+    console.log(impId)
+    var cmtText = document.getElementById(`content#${impId}`).value
+    if (cmtText != "") {
+        document.getElementById(`cmt#${impId}`).innerHTML += `
+        <div class="cmt-text" id="cmted#3">
+        <img src="./asset/profile-pink.png" alt="profile">
+        <span> ${cmtText}
+        </span>
+        </div>
+        `
+    } else {
+        console.log('Comment has no content! Send denied')
+    }
 }
