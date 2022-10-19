49d57c16b0ade730ae38e726df864b21a5a20cff
LifeGoal <54373229+SeakMengs@users.noreply.github.com>
2022-10-20 04:11:21 +0700

    update comment and render

--- script/confession.js
+++ script/confession.js
@@ -1,5 +1,3 @@
-
-
 var postData = []
 const profile = [
     "./asset/profile-black.png",
@@ -10,9 +8,11 @@ const profile = [
     "./asset/profile-green.png"
 ]
 
+// ? when window on load we generate posts and comments
 window.onload = async function () {
     generateProfile()
     await generateData()
+    generateComment()
 }
 
 function fetchData() {
@@ -23,16 +23,15 @@ function fetchData() {
     })
 }
 
-function generateData() {
+async function generateData() {
     postData = []
-    fetchData().then(function (result) {
+    await fetchData().then(function (result) {
         for (let i = result.length - 1; i >= 0; i--) {
             postData.push(result[i]);
         }
     }).then(() => {
         // ? after pushing data to our array we start generate the post
         console.log(postData)
-        // generateProfile()
         genertatePost()
     })
 }
@@ -57,18 +56,18 @@ function genertatePost() {
                     <div class="reaction">
                         <div class="sus reaction-align">
                             <img src="./asset/sus.png" alt="reaction">
-                            <span class="sus-reaction">${postData[i].upvote}</span>
+                            <span class="sus-reaction" id="upvote#${postData[i].impostorId}">${postData[i].upvote}</span>
                         </div>
                         <div class="downvote reaction-align">
                             <img src="./asset/voted.png" alt="reaction">
-                            <span class="vote-reaction">${postData[i].downvote}</span>
+                            <span class="vote-reaction" id="downvote#${postData[i].impostorId}">${postData[i].downvote}</span>
                         </div>
                         <div class="comment reaction-align">
-                            <span class="comment-btn">Comment</span>
+                            <span class="comment-btn" id="comment#${postData[i].impostorId}">0 Comment</span>
                         </div>
                         <div class="report reaction-align">
                             <img src="./asset/report.png" alt="reaction">
-                            <span class="report-reaction">${postData[i].share}</span>
+                            <span class="report-reaction" id="share#${postData[i].impostorId}">${postData[i].share}</span>
                         </div>
                     </div>
                     <div class="comment-area">
@@ -101,18 +100,18 @@ function genertatePost() {
                     <div class="reaction">
                         <div class="sus reaction-align">
                             <img src="./asset/sus.png" alt="reaction">
-                            <span class="sus-reaction">${postData[i].upvote}</span>
+                            <span class="sus-reaction" id="upvote#${postData[i].impostorId}">${postData[i].upvote}</span>
                         </div>
                         <div class="downvote reaction-align">
                             <img src="./asset/voted.png" alt="reaction">
-                            <span class="vote-reaction">${postData[i].downvote}</span>
+                            <span class="vote-reaction" id="downvote#${postData[i].impostorId}">${postData[i].downvote}</span>
                         </div>
                         <div class="comment reaction-align">
-                            <span class="comment-btn">Comment</span>
+                            <span class="comment-btn" id="comment#${postData[i].impostorId}">0 Comment</span>
                         </div>
                         <div class="report reaction-align">
                             <img src="./asset/report.png" alt="reaction">
-                            <span class="report-reaction">${postData[i].share}</span>
+                            <span class="report-reaction" id="share#${postData[i].impostorId}">${postData[i].share}</span>
                         </div>
                     </div>
                     <div class="comment-area">
@@ -136,6 +135,40 @@ function generateProfile() {
     document.querySelector('.prof-logo').src = profile[randomNum(0, 5)]
 }
 
+function generateComment() {
+    for (var i = postData.length - 1; i >= 0; i--) {
+        // console.log(postData[i].comment)
+
+        //* https://stackoverflow.com/questions/24403732/how-to-check-if-array-is-empty-or-does-not-exist
+
+        if (postData[i].comment?.length) {
+            // ? check size of comment then proceed to render comment
+            // console.log(postData[i].comment)
+            for (var j = 0; j < postData[i].comment.length; j++) {
+                var tempText = JSON.stringify(postData[i].comment[j].comments)
+                // ? remove first and last character
+                tempText = tempText.substring(1, tempText.length - 1)
+
+                document.getElementById(`cmt#${postData[i].impostorId}`).innerHTML += `
+                <div class="cmt-text" id="cmted#${j + 1}">
+                <img src="${profile[randomNum(0, 5)]}" alt="profile">
+                <span> ${tempText}
+                </span>
+                </div>
+                `
+
+                tempText = ''
+            }
+            if (postData[i].comment.length == 0 || postData[i].comment.length == 1) {
+                document.getElementById(`comment#${postData[i].impostorId}`).innerHTML = postData[i].comment.length + " comment"
+            } else if (postData[i].comment.length > 1) {
+                document.getElementById(`comment#${postData[i].impostorId}`).innerHTML = postData[i].comment.length + " comments"
+            }
+        }
+    }
+    console.log("Done")
+}
+
 //*Credit https://stackoverflow.com/questions/17772260/textarea-auto-height
 function auto_grow(element) {
     element.style.height = "5px";
@@ -143,14 +176,42 @@ function auto_grow(element) {
 }
 
 
-function sendCmt(event) {
+async function sendCmt(event) {
+    var commentArr = [{}]
+    var textArr = {}
+    var cmtId = ''
     var ranNum = randomNum(0, 5)
-    var impId = event.replace('send#', '');
+    var impId = event.replace('send#', '')
+    var totalCmt = ''
     // console.log(impId)
     var cmtText = document.getElementById(`content#${impId}`).value
     if (cmtText != "") {
+        // var commentArr = [{}]
+        await fetchData().then(function (allData) {
+
+            // ? get array of comment and then we put object into temp array then push to array of comment and start patching on database
+            commentArr = allData[impId - 1].comment
+            // console.log(commentArr)
+            textArr = { comments: cmtText }
+            commentArr.push(textArr)
+            cmtId = commentArr.length
+            console.log(commentArr)
+        })
+
+        try {
+            await axios.patch(`https://yatoconfessionapi77.netlify.app/.netlify/functions/api/post/by_id/${impId}`, {
+                comment: commentArr
+            }).then((res) => {
+                console.log("Comment has been posted")
+                // console.log(res)
+                return res
+            })
+        } catch (err) {
+            console.log(err)
+        }
+
         document.getElementById(`cmt#${impId}`).innerHTML += `
-        <div class="cmt-text" id="cmted#3">
+        <div class="cmt-text" id="cmted#${cmtId}">
         <img src="${profile[ranNum]}" alt="profile">
         <span> ${cmtText}
         </span>
@@ -159,8 +220,21 @@ function sendCmt(event) {
     } else {
         console.log('Comment has no content! Send denied')
     }
+
+    // ? after post to api we change text area to blank and change comment plural
+
     document.getElementById(`content#${impId}`).value = ''
     document.getElementById(`content#${impId}`).style.height = '20px'
+
+    totalCmt = document.getElementById(`comment#${impId}`).innerText
+    totalCmt = totalCmt.replace(' comments', '')
+    totalCmt = parseInt(totalCmt)
+
+    if (totalCmt <= 1) {
+        document.getElementById(`comment#${impId}`).innerHTML = parseInt(1 + totalCmt) + " comment"
+    } else if (totalCmt > 1) {
+        document.getElementById(`comment#${impId}`).innerHTML = parseInt(1 + totalCmt) + " comments"
+    }
 }
 
 async function postContent(data) {
@@ -185,7 +259,7 @@ async function postContent(data) {
             console.log("API has been posted")
             return res
         })
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     }
 }
@@ -219,8 +293,8 @@ async function confess() {
         // ? wait for content to store in database before render to the client postContent(contentData)
         //* https://laracasts.com/discuss/channels/javascript/how-to-properly-use-async-await-with-axios-post-request
         const apiPost = await postContent(contentData)
-            document.getElementById('confess-box').value = ''
-            document.querySelector('.post-container').innerHTML = ''
-            generateData()
+        document.getElementById('confess-box').value = ''
+        document.querySelector('.post-container').innerHTML = ''
+        generateData()
     }
 }
